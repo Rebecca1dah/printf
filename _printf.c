3642a280733ad36134868178e0ce1f8eb652db12
Rebecca1dah <rebeccadahzannou62@gmail.com>
2023-09-20 05:27:24 -0700

    mes fonctions

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * _printf - print to stout formated text
+ *
+ * @format: format specifier
+ * Return: no of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+        unsigned int i, count = 0;
+        va_list args;
+        va_start(args, format);
+
+        for (i = 0; format[i] != "\0"; i++)
+        {
+		if(format[i] != '%')
+		{
+			putchar(format[i]);
+		}
+        }
+
+
+}
---
+++ putchr.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * putchr - print a character
+ * @c: char input
+ * Return: 1
+ */
+int putchr(char c)
+{
+	return (write(1, &c, 1));
+}
